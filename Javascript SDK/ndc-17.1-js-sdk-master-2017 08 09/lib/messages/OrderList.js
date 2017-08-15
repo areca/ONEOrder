d7bdc306bba38fea7af1242729bc1703591ba707
Tibo <ruyt@iata.org>
2017-08-15 23:05:35 +0200

    OrderList sample
    This sample allows to retrieve a list of Orders matching filters

--- Javascript SDK/ndc-17.1-js-sdk-master-2017 08 09/lib/messages/OrderList.js
+++ Javascript SDK/ndc-17.1-js-sdk-master-2017 08 09/lib/messages/OrderList.js
@@ -4,17 +4,18 @@ var common = require('./common');
 
 var OrderList = function(data) {
     return {
-        PointOfSale: common.PointOfSale(data),
-        Document: common.Document(data),
-        Party: common.Party(data, true),
-        Query: {
-            Filters: {
-                Airline: {
-                    AirlineID: data.filter.airlineID
-                }
-            }
-        }
-    };
+	PointOfSale: common.PointOfSale(data),
+	Document: common.Document(data),
+	Party: common.Party(data,
+	true),
+	Query: {
+		Filters: {
+			Airline: [{
+				AirlineID: data.Filters[0].airlineID
+			}]
+		}
+	}
+};
 };
 
 module.exports = OrderList;
